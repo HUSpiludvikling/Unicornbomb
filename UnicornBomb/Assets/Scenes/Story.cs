0d3d94a3983aa87b492ea5aec972125f882e9827
Stine <sle3108@gmail.com>
2019-02-27 16:26:55 +0100

    Tilføjede story script og scene
    -Story klip
    - Story reader
    - Cut_scene_scene

---
+++ UnicornBomb/Assets/Scenes/Story.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[CreateAssetMenu(fileName = "Blank Story", menuName = "Story")]
+public class Story : ScriptableObject
+{
+    public List<string> Linjer_af_tekst;
+    public List<Sprite> Billeder;
+
+
+}
---
+++ UnicornBomb/Assets/Scenes/Story_reader.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Story_reader : MonoBehaviour
+{
+    public Story story;
+}
