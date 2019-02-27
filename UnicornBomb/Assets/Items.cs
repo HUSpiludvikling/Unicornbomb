fff160d70836525ec7858ac0d3ce20d6749e4db2
Jasmin <jasminrasmussen3004@gmail.com>
2019-02-27 16:06:29 +0100

    tilføjet item script og design mappe

---
+++ UnicornBomb/Assets/Items.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Blank Item", menuName = "Items")]
+public class Items : ScriptableObject
+{
+    public Color itemColor = Color.red;
+        public string itemName = "Blank";
+    public Sprite image;
+}
