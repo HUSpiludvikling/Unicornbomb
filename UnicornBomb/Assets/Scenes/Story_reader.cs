e40e3a20d9b027d7d1347b36c2422d1ca3f9e224
Stine <sle3108@gmail.com>
2019-02-27 17:00:40 +0100

    rettet et eller andet

--- UnicornBomb/Assets/Scenes/Story_reader.cs
+++ UnicornBomb/Assets/Scenes/Story_reader.cs
@@ -1,8 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+/// <summary>
+/// Læser story og reflekterer det i tekst og billede
+/// </summary>
 public class Story_reader : MonoBehaviour
-{
+{   //reference til en story//
     public Story story;
+    private Text text;
+    private Image image;
+
+    int index = default(int);
+
+    private void Start()
+    {
+        text = GetComponentInChildren<Text>();
+        image = GetComponentInChildren<Image>();
+        SetTextImage();
+        
+
+    }
+
+    private void SetTextImage()
+    {
+        text.text = story.Linjer_af_tekst[0];
+        image.sprite = story.Billeder[0];
+    }
 }
+
