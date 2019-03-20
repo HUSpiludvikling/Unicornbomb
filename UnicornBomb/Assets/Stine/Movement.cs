592b92af890a58c2b65de2fad629829cb4d4b46d
Stine <sle3108@gmail.com>
2019-03-20 15:06:31 +0100

    Alt mugligt
    movement
    physics material

---
+++ UnicornBomb/Assets/Stine/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody2D Rb;
+    [Range(0.5f,40f)]
+    public float SpeedMod;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            Rb.AddForce(Vector2.left * -Input.GetAxis("Horizontal") * SpeedMod, ForceMode2D.Force);
+        } 
+    }
+}
